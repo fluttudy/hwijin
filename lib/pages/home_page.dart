69e95efa9eeeab6cb8dbba1aa8aa0f43cd771ce1
KimHwijin <gnlwls623@daum.net>
2024-09-30 18:43:29 +0900

    current location

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_map_app/controller/search_notifier.dart';
 import 'package:google_map_app/pages/search_page.dart';
 import 'package:google_map_app/services/palce_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PlacesSearchScreen extends ConsumerWidget {
+  PlacesSearchScreen({Key? key}) : super(key: key);
+
   final PlacesService placesService =
       PlacesService('AIzaSyAKYvan7-mVt87Rg7h0fh-aISrKChM-JX0');
 
@@ -14,7 +17,32 @@ class PlacesSearchScreen extends ConsumerWidget {
 
   FocusNode textFocus = FocusNode();
 
-  PlacesSearchScreen({Key? key}) : super(key: key);
+  Future<LatLng> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied.');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied.');
+    }
+
+    // 현재 위치 가져오기
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    return LatLng(position.latitude, position.longitude);
+  }
 
   void _showBottomSheet(
       BuildContext context, Map<String, dynamic> placeDetails) {
@@ -77,59 +105,70 @@ class PlacesSearchScreen extends ConsumerWidget {
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      behavior: HitTestBehavior.opaque, // HitTestBehavior 설정
+      behavior: HitTestBehavior.opaque,
       child: Scaffold(
-        body: Stack(
-          children: [
-            GoogleMap(
-              onMapCreated: (controller) {
-                mapController = controller;
-              },
-              initialCameraPosition: const CameraPosition(
-                target: LatLng(37.7749, -122.4194), // San Francisco
-                zoom: 18,
-              ),
-            ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: SearchBar(
-                    controller: searchList,
-                    focusNode: textFocus,
-                    onTap: () async {
-                      final selectedPlace = await Navigator.push(
-                        context,
-                        PageRouteBuilder(
-                          pageBuilder: (BuildContext context,
-                              Animation<double> animation1,
-                              Animation<double> animation2) {
-                            return const SearchPage(); //변경 필요
-                          },
-                          transitionDuration: Duration.zero,
-                          reverseTransitionDuration: Duration.zero,
-                        ),
-                      );
+        body: FutureBuilder<LatLng>(
+            future: _getCurrentLocation(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                return Stack(
+                  children: [
+                    GoogleMap(
+                      onMapCreated: (controller) {
+                        mapController = controller;
+                      },
+                      initialCameraPosition: CameraPosition(
+                        target: snapshot.data!,
+                        zoom: 18,
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: SearchBar(
+                            controller: searchList,
+                            focusNode: textFocus,
+                            onTap: () async {
+                              final selectedPlace = await Navigator.push(
+                                context,
+                                PageRouteBuilder(
+                                  pageBuilder: (BuildContext context,
+                                      Animation<double> animation1,
+                                      Animation<double> animation2) {
+                                    return const SearchPage();
+                                  },
+                                  transitionDuration: Duration.zero,
+                                  reverseTransitionDuration: Duration.zero,
+                                ),
+                              );
 
-                      if (selectedPlace != null) {
-                        final location = LatLng(
-                          selectedPlace['geometry']['location']['lat'],
-                          selectedPlace['geometry']['location']['lng'],
-                        );
-                        print(
-                            'Moving to: ${location.latitude}, ${location.longitude}');
-                        await _moveToLocation(location);
-                        _showBottomSheet(context, selectedPlace);
-                      }
-                    },
-                    hintText: 'Search places...',
-                    backgroundColor: const WidgetStatePropertyAll(Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                              if (selectedPlace != null) {
+                                final location = LatLng(
+                                  selectedPlace['geometry']['location']['lat'],
+                                  selectedPlace['geometry']['location']['lng'],
+                                );
+                                print(
+                                    'Moving to: ${location.latitude}, ${location.longitude}');
+                                await _moveToLocation(location);
+                                _showBottomSheet(context, selectedPlace);
+                              }
+                            },
+                            hintText: 'Search places...',
+                            backgroundColor:
+                                const WidgetStatePropertyAll(Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              }
+            }),
       ),
     );
   }
